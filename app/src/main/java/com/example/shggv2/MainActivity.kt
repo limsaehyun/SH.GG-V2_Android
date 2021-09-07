83763593e8a24d2b408184911ff284bfcc0c36e0
limsaehyun <sh00710000@gmail.com>
2021-09-07 19:30:56 +0900

    Fix :: viewBinding

--- app/src/main/java/com/example/shggv2/MainActivity.kt
+++ app/src/main/java/com/example/shggv2/MainActivity.kt
@@ -7,13 +7,18 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.shggv2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
