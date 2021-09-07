3d942613d180bb95a5644c1862b2a0d0f3705d6c
limsaehyun <sh00710000@gmail.com>
2021-09-07 19:41:16 +0900

    Feat :: move UserPage

--- app/src/main/java/com/example/shggv2/ui/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/shggv2/ui/fragment/SearchFragment.kt
@@ -1,14 +1,18 @@
 package com.example.shggv2.ui.fragment
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.shggv2.R
+import com.example.shggv2.UserActivity
 import com.example.shggv2.databinding.FragmentRankBinding
 import com.example.shggv2.databinding.FragmentSearchBinding
 
@@ -21,7 +25,15 @@ class SearchFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentSearchBinding.inflate(inflater, container, false);
+        val binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        binding.ibSearch.setOnClickListener {
+            if(binding.etUserName.text.isEmpty()) {
+                Toast.makeText(context, "소환사 닉네임을 입력해주세요!", Toast.LENGTH_SHORT).show()
+            } else {
+                startActivity(Intent(context, UserActivity::class.java))
+            }
+        }
 
         return binding.root
     }
