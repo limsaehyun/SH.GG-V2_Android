c4decccf18be9c77cd04dbcfcb3417bbb06924ac
limsaehyun <sh00710000@gmail.com>
2021-09-07 19:41:04 +0900

    Feat :: create UserPage

---
+++ app/src/main/java/com/example/shggv2/UserActivity.kt
@@ -0,0 +1,11 @@
+package com.example.shggv2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
