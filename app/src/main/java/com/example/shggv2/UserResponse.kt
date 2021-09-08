42dcf077f27114a6636d3312f7c769dc42593739
limsaehyun <sh00710000@gmail.com>
2021-09-08 20:10:38 +0900

    Feat :: UserInfo Response

---
+++ app/src/main/java/com/example/shggv2/UserResponse.kt
@@ -0,0 +1,4 @@
+package com.example.shggv2
+
+data class UserResponse(val tier: List<String>, val rank: List<String>, val summonerName: List<String>,
+                        val wins: List<String>, val losses: List<String>)
