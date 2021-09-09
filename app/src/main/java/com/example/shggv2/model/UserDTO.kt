ef9d873256c4e8fe13d99dfb466e9e02dc51a1b1
limsaehyun <sh00710000@gmail.com>
2021-09-09 15:20:57 +0900

    Fix :: type change

--- app/src/main/java/com/example/shggv2/model/UserDTO.kt
+++ app/src/main/java/com/example/shggv2/model/UserDTO.kt
@@ -1,4 +1,4 @@
 package com.example.shggv2.model
 
 data class UserDTO(val tier: String, val rank: String, val summonerName: String,
-                   val wins: String, val losses: String)
+                   val wins: Int, val losses: Int, val leaguePoints: Int)
