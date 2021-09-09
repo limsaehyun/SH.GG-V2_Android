3e79f5e4e0ddd81a4c463d7f3ddfb7049eb1e2cf
limsaehyun <sh00710000@gmail.com>
2021-09-09 18:47:12 +0900

    Fix :: unnecessary data delete

--- app/src/main/java/com/example/shggv2/model/SummonerDTO.kt
+++ app/src/main/java/com/example/shggv2/model/SummonerDTO.kt
@@ -1,5 +1,3 @@
 package com.example.shggv2.model
 
-data class SummonerDTO(val id: String, val accountId: String,
-                       val puuid: String, val name: String, val profileIconId: String,
-                       val revisionDate: String, val summonerLevel: String)
+data class SummonerDTO(val id: String, val profileIconId: String)
