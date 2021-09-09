1560b10362b251d5941c430bcd749d1bd76ae4bd
limsaehyun <sh00710000@gmail.com>
2021-09-09 23:22:37 +0900

    Feat :: RankDTO

---
+++ app/src/main/java/com/example/shggv2/model/RankData.kt
@@ -0,0 +1,4 @@
+package com.example.shggv2.model
+
+data class RankData(val tier: String, val rank: String, val summonerName: String, val leaguePoints: Int,
+                    val wins: Int, val losses: Int)
