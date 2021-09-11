f517b88ec5c4c8d165da8c9d7d0d8ccd2cb17fdf
limsaehyun <sh00710000@gmail.com>
2021-09-11 22:15:29 +0900

    Feat :: create RankRvAdapter

--- app/src/main/java/com/example/shggv2/remote/RankAdapter.kt
+++ app/src/main/java/com/example/shggv2/remote/RankAdapter.kt
@@ -1,37 +1,45 @@
 package com.example.shggv2.remote
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.SimpleAdapter
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shggv2.R
+import com.example.shggv2.model.RankDTO
+import com.example.shggv2.model.RankData
+import com.example.shggv2.model.UserDTO
 
 
-class RankAdapter(private val dataSet: Array<String>) :
-    RecyclerView.Adapter<RankAdapter.ViewHolder>() {
+class RankAdapter(val context: Context, val studentList: ArrayList<RankDTO>):
+        RecyclerView.Adapter<RankAdapter.Holder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView
+    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+        val tvRankCount = itemView?.findViewById<TextView>(R.id.tvRankCount)
+        val ivRankUserImage = itemView?.findViewById<ImageView>(R.id.ivRankUserImage)
+        val tvRankUserName = itemView?.findViewById<TextView>(R.id.tvRankUserName)
+        val ivRanTierBg = itemView?.findViewById<ImageView>(R.id.ivRanTierBg)
+        val tvRankUserTier = itemView?.findViewById<TextView>(R.id.tvRankUserTier)
+        val tvRankLP = itemView?.findViewById<TextView>(R.id.tvRankLP)
 
-        init {
-            textView = view.findViewById(R.id.textView)
+        fun bind(rankDTO: RankDTO, context: Context) {
         }
     }
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.rank_view, viewGroup, false)
-
-        return ViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(context).inflate(R.layout.rank_view, parent, false)
+        return Holder(view)
     }
 
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-
-        viewHolder.textView.text = dataSet[position]
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(studentList[position], context)
     }
 
-    override fun getItemCount() = dataSet.size
-
+    override fun getItemCount(): Int {
+        return studentList.size
+    }
 }
