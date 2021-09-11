1e1a00717342c6635613ed8217c1ba1042f20069
limsaehyun <sh00710000@gmail.com>
2021-09-11 22:16:21 +0900

    Feat :: getRanking

--- app/src/main/java/com/example/shggv2/ui/fragment/RankFragment.kt
+++ app/src/main/java/com/example/shggv2/ui/fragment/RankFragment.kt
@@ -1,6 +1,7 @@
 package com.example.shggv2.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,12 +9,32 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shggv2.R
 import com.example.shggv2.databinding.FragmentRankBinding
+import com.example.shggv2.databinding.RankViewBinding
+import com.example.shggv2.model.RankDTO
+import com.example.shggv2.model.RankData
+import com.example.shggv2.model.UserDTO
+import com.example.shggv2.remote.ApiProvider
+import com.example.shggv2.remote.RankAdapter
+import com.example.shggv2.remote.RiotAPI
+import com.example.shggv2.remote.api_key
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RankFragment : Fragment() {
 
+    private val TAG = "RankFragment"
+    
     private lateinit var binding: FragmentRankBinding
+
+    private lateinit var rvRank: RecyclerView
+
+    private var rankList = arrayListOf<RankDTO>()
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -22,6 +43,38 @@ class RankFragment : Fragment() {
     ): View? {
         val binding = FragmentRankBinding.inflate(inflater, container, false)
 
+
+        binding.rvRank.adapter = context?.let { RankAdapter(it, rankList) }
+        binding.rvRank.layoutManager = LinearLayoutManager(context)
+
+        binding.rvRank.setHasFixedSize(true)
+
+
+        binding.tvSoloRank.setOnClickListener {
+            getRank("RANKED_SOLO_5x5", "CHALLENGER", "I")
+        }
+
         return binding.root
+    }
+
+    private fun getRank(queue: String, tier: String, division: String) {
+        val apiProvider = ApiProvider.getInstance().create(RiotAPI::class.java)
+
+        val call: Call<List<RankDTO>> = apiProvider.getRank(api_key, queue, tier, division)
+
+        call.enqueue(object : Callback<List<RankDTO>> {
+            override fun onResponse(call: Call<List<RankDTO>>, response: Response<List<RankDTO>>) {
+                Log.d(TAG, "onResponse: ")
+            }
+
+            override fun onFailure(call: Call<List<RankDTO>>, t: Throwable) {
+                Log.d(TAG, "onFailure: ")
+            }
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        
     }
 }
