417862e20971dea6ffa6aef1bbf15f6246f84907
limsaehyun <sh00710000@gmail.com>
2021-09-11 22:14:33 +0900

    Feat :: Rank DTO

---
+++ app/src/main/java/com/example/shggv2/model/RankDTO.kt
@@ -0,0 +1,6 @@
+package com.example.shggv2.model
+
+data class RankDTO(val leagueId: String, val queueType: String, val tier: String, val rank: String,
+                    val summonerId: String, val summonerName: String, val leaguePoints: Int,
+                    val wins: Int, val losses: Int, val veteran: Boolean, val inactive: Boolean,
+                    val freshBlood: Boolean, val hotStreak: Boolean)
