df797d1f4477603b6b8a86c047faa51d0cc83b6a
limsaehyun <sh00710000@gmail.com>
2021-09-11 23:04:55 +0900

    Feat :: set adapter tv

--- app/src/main/java/com/example/shggv2/remote/RankAdapter.kt
+++ app/src/main/java/com/example/shggv2/remote/RankAdapter.kt
@@ -4,10 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.SimpleAdapter
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shggv2.R
 import com.example.shggv2.model.RankDTO
@@ -15,18 +12,22 @@ import com.example.shggv2.model.RankData
 import com.example.shggv2.model.UserDTO
 
 
-class RankAdapter(val context: Context, val studentList: ArrayList<RankDTO>):
+class RankAdapter(val context: Context, val studentList: ArrayList<RankData>):
         RecyclerView.Adapter<RankAdapter.Holder>() {
 
     inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
         val tvRankCount = itemView?.findViewById<TextView>(R.id.tvRankCount)
         val ivRankUserImage = itemView?.findViewById<ImageView>(R.id.ivRankUserImage)
         val tvRankUserName = itemView?.findViewById<TextView>(R.id.tvRankUserName)
-        val ivRanTierBg = itemView?.findViewById<ImageView>(R.id.ivRanTierBg)
+        val ivRanTierBg = itemView?.findViewById<RelativeLayout>(R.id.ivRanTierBg)
         val tvRankUserTier = itemView?.findViewById<TextView>(R.id.tvRankUserTier)
         val tvRankLP = itemView?.findViewById<TextView>(R.id.tvRankLP)
 
-        fun bind(rankDTO: RankDTO, context: Context) {
+        fun bind(rankData: RankData, context: Context) {
+            tvRankCount?.text = rankData.rank.toString()
+            tvRankUserName?.text = rankData.summonerName
+            tvRankUserTier?.text = rankData.tier
+            tvRankLP?.text = rankData.leaguePoints.toString()
         }
     }
 
