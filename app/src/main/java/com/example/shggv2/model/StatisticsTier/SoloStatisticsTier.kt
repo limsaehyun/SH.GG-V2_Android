57a616ef261c931890ff20f24768294c78b8d95e
limsaehyun <sh00710000@gmail.com>
2021-09-26 23:22:21 +0900

    Feat :: statistics dataClass

---
+++ app/src/main/java/com/example/shggv2/model/StatisticsTier/SoloStatisticsTier.kt
@@ -0,0 +1,6 @@
+package com.example.shggv2.model.StatisticsTier
+
+class SoloStatisticsTier (
+        val test: String = "테스트",
+        val soloMap: Map<String, String> = mapOf("테스트1" to "테스트입니다")
+)
